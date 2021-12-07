8fa7d1b12529504149fb0c91c473a356c1366fb7
Enes Karakaş <enes.karakas.e@gamil.com>
2021-12-07 14:47:21 +0300

    day 1, part 2 solution

--- day1/index.js
+++ day1/index.js
@@ -4,22 +4,39 @@ const data = fs
   .readFileSync(path.join(`${__dirname}`, 'input.txt'), 'utf-8')
   .split('\n');
 
-function countIncreasedNumbers() {
-  let countLargerThanPrevious = 0;
-  const solution = data.map((number, index) => {
-    if (index === 0) {
-      return number + ' (N/A - no previous measurement)\n';
-    }
-
-    if (number > data[index - 1]) {
-      countLargerThanPrevious++;
-      return number + ' (increased)\n';
-    } else {
-      return number + ' (decreased)\n';
+//part1
+function countIncreasedNumbers(data) {
+  let c = 0;
+  data.forEach((number, index) => {
+    if (index === 0) return;
+    if (parseInt(number) > parseInt(data[index - 1])) {
+      c++;
     }
   });
 
-  return countLargerThanPrevious;
+  return c;
 }
 
-console.log('countIncreasedNumbers: ', countIncreasedNumbers());
+//part2
+function creatingMainWindowArray(data) {
+  const dataLength = data.length;
+  console.log(dataLength);
+
+  return data
+    .map((number, index) => {
+      if (dataLength - index >= 2) {
+        return (
+          parseInt(number) +
+          parseInt(data[index + 1]) +
+          parseInt(data[index + 2])
+        );
+      }
+    })
+    .filter((n) => n);
+}
+
+console.log('countIncreasedNumbers: ', countIncreasedNumbers(data));
+console.log(
+  'part2 Solution: ',
+  countIncreasedNumbers(creatingMainWindowArray(data))
+);
